ebfaef990d212d90be28d6ab42d9382b76afb909
Natthasath Saksupanara <natthasath.sak@gmail.com>
2022-08-16 21:37:28 +0700

    First Commit

---
+++ check_tensorflow.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+print(tf.__version__)
+
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
